f7a17fd06115d5b19be14d19c2643232efdf62b0
reza yeganeh <r.yeganeh85@gmail.com>
2024-05-14 17:41:33 +0330

    pretty syntax added to chain

--- src/chain/chain.service.spec.ts
+++ src/chain/chain.service.spec.ts
@@ -21,9 +21,9 @@ describe('ChainService', () => {
 
   it('should demonstrate chain of responsibilities', () => {
     // Building the chain
-    const junior = new JuniorSupportHandler();
-    const senior = junior.setNext(new SeniorSupportHandler());
-    senior.setNext(new ManagerSupportHandler());
+    const junior = new JuniorSupportHandler()
+      .setNext(new SeniorSupportHandler())
+      .setNext(new ManagerSupportHandler());
     // run requests
     const requests = ['Simple Query', 'Complex Query', 'High-Level Issue'];
     service.clientCode(requests, junior);
